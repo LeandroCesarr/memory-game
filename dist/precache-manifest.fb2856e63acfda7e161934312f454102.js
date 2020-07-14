f7a92500cb95e66dfe9a2733042d437fea6bb11d
Leandro C. Silva <leandro.cesar@novahaus.com.br>
2020-07-14 10:08:41 -0300

    Add dist folder to view on github pages

---
+++ dist/precache-manifest.fb2856e63acfda7e161934312f454102.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95246ffc2d0b88c417a4",
+    "url": "/css/app.ab25a209.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "88f089572139d6128685681114bd2190",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95246ffc2d0b88c417a4",
+    "url": "/js/app.f7692443.js"
+  },
+  {
+    "revision": "9e83726d6b1a868d032f",
+    "url": "/js/chunk-vendors.bdc9172a.js"
+  },
+  {
+    "revision": "8dc341de3428049269eec4dcdd9257f4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
